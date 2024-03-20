e5226a471e3309585ab379b548e442da16b7a4b7
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-03-21 00:34:41 +0900

    init svelte

---
+++ front/src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Router, Link, Route } from "svelte-routing";
+
+  import Home from './routes/+page.svelte';
+  import NotFound from './routes/+error.svelte';
+
+  export let url = "";
+</script>
+
+<!-- https://github.com/EmilTholin/svelte-routing/issues/18 -->
+
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route><NotFound /></Route>
+  </div>
+</Router>
